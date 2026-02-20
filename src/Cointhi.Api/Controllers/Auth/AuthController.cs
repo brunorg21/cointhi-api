ae0ba282fa3c7b9b00dafbc02b5e2d6e7166f87f
Bruno Rafael <rbrunog21@gmail.com>
2026-02-19 22:49:27 -0300

    refactor: add base const to route

--- src/Cointhi.Api/Controllers/Auth/AuthController.cs
+++ src/Cointhi.Api/Controllers/Auth/AuthController.cs
@@ -1,5 +1,4 @@
 ﻿using Cointhi.Api.DTOs.Requests;
-using Cointhi.Api.Exceptions;
 using Cointhi.Api.UseCases.Auth.Login;
 using Cointhi.Api.UseCases.Auth.Register;
 
@@ -7,9 +6,10 @@ namespace Cointhi.Api.Controllers.Auth
 {
     public static class MapAuthController
     {
+        private const string BASE_ROUTE = "/auth";
         public static void MapAuthRoutesApi(this IEndpointRouteBuilder app)
         {
-            var authRouteGroup = app.MapGroup("/auth");
+            var authRouteGroup = app.MapGroup(BASE_ROUTE);
 
             authRouteGroup.MapPost("/register", async (RegisterUserRequest request, IRegisterUserUseCase useCase, CancellationToken cancellationToken) =>
             {
